4e942ee53c3ba3b5897b596bf069d6e065e64be4
Durga <dkanakadurga@gmail.com>
2019-08-07 16:32:27 +0100

    sent 100 parallel requests to the server using web workers

--- web-worker.js
+++ web-worker.js
@@ -1,22 +1,22 @@
+self.addEventListener('message', (ev) => {
+    console.log(ev.data);
+   var resMsg = HTTPreq();
+    self.postMessage('received ' +resMsg)
+})
 
+function HTTPreq() {
+    var params = JSON.stringify({
+               first: 0,
+               second: 1,
+               third: 2
+           });
+        var url = "https://qcvault.herokuapp.com/unlock_safe";
+        var xmlHttp = new XMLHttpRequest();
+         xmlHttp.open('POST', url, false);
+         xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+         xmlHttp.send(params)
+       var resData = xmlHttp.responseText;
+       console.log(resData);
+       return resData;
+} 
 
-    self.onmessage = function (e) {
-        var param = e.data;
-        var url="https://qcvault.herokuapp.com/unlock_safe";
-        var data=getSpecData(param.detailLvl,param.startWeek,param.endWeek,param.mkt,url)
-        self.postMessage(data);
-    }; 
-    function getSpecData(detailLvl, startWeek, endWeek, mkt, url) {
-       var xmlHttp = new XMLHttpRequest();
-       var params = JSON.stringify({
-           first: 0,
-           second: 1,
-           third: 2
-       });
-       
-       xmlHttp.open('POST', url, false);
-       xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xmlHttp.send(params)
-
-
-    }
